96038174982cf1a94b80e2cb37aa7d2ba5b782e2
R7558 <rupsanaworld1004@gmail.co>
2023-05-18 14:07:49 +0600

    added appointment

--- src/pages/Appointment/AppointmentBanner/AppointmentBanner.js
+++ src/pages/Appointment/AppointmentBanner/AppointmentBanner.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
+import chair from '../../../assets/images/chair.png'
+const AppointmentBanner = ({selectedDate, setSelectedDate}) => {
 
-const AppointmentBanner = () => {
     return (
-        <div>
-            <h1>This is Appointment Banner</h1>
+        <header className='my-6'>
+        <div className="hero">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img src={chair} alt="dentist chair" className="max-w-sm rounded-lg shadow-2xl" />
+                <div className='mr-6 sm:w-full'>
+                    <DayPicker 
+                        mode='single'
+                        selected={selectedDate}
+                        onSelect={setSelectedDate}
+                    />
+                </div>
+            </div>
         </div>
+    </header>
     );
 };
 
