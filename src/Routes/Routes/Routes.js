683cba3dcccf727b80d6f2a44ce56ae79b01bd4a
R7558 <rupsanaworld1004@gmail.co>
2023-05-21 19:19:37 +0600

    added login

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -4,6 +4,9 @@ import Main from "../../Layout/Main";
 import Home from "../../pages/Home/Home/Home";
 import Login from "../../pages/Login/Login";
 import Appointment from "../../pages/Appointment/Appointment/Appointment";
+import PrivateRoutes from "../PrivateRoutes/PrivateRoutes";
+import DashBoard from "../../pages/DashBoard/DashBoard/DashBoard";
+import SignUp from "../../pages/SignUp/SignUp";
 
 
 
@@ -21,10 +24,18 @@ import Appointment from "../../pages/Appointment/Appointment/Appointment";
                 element:<Login></Login>
             },
             {
+                path: '/signup',
+                element:<SignUp></SignUp>
+            },
+            {
                 path: '/appointment',
                 element:<Appointment></Appointment>
             } 
         ]
+    },
+    {
+        path: '/dashboard',
+        element: <PrivateRoutes><DashBoard></DashBoard></PrivateRoutes>
     }
 ])
 
